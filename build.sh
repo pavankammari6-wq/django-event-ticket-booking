f533321282f9d8137dfef3629e51bd21940480c5
pavan@gmail.com <pavan@gmail.com>
2026-07-23 21:34:20 +0530

    for createsuperuser

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; import os; User = get_user_model(); u=os.environ.get('DJANGO_SUPERUSER_USERNAME'); p=os.environ.get('DJANGO_SUPERUSER_PASSWORD'); e=os.environ.get('DJANGO_SUPERUSER_EMAIL',''); (not u or not p) or User.objects.filter(username=u).exists() or User.objects.create_superuser(u, e, p)"
